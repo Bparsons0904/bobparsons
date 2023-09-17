bb9bdc16be899e61d7e345fb165d67d01ee23646
Bob Parsons <deadstylebp@gmail.com>
2023-09-17 18:53:31 -0500

    Header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,39 @@
 	<a href="/about">About</a>
 </nav>
 
+
+
 <slot></slot>
+
+<style lang="scss">
+     :global(body){
+        background-color: #000;
+    }
+
+    // Set root color variables
+    :root {
+        --color-primary: #fca311;
+        --color-secondary-light: #e5e5e5;
+        --color-secondary-dark: #14213d;
+        --color-text-light: #ffffff;
+        --color-text-dark: #000000;
+    }
+
+    :global(.btn) {
+        padding:1.5rem;
+        border: 3px solid var(--color-primary);
+        border-radius: 25px;
+    }
+    
+    :global(.btn-primary) {
+        font-size: 1.5rem;
+        background-color: var(--color-secondary-dark);
+        color: var(--color-text-light);
+        transition: all 0.3s ease-in-out;
+    }
+
+    :global(.btn-primary:hover) {
+        background-color: var(--color-primary);
+        color: var(--color-text-dark);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section>
+    <div class="intro-image-wrapper">
+        <div class="intro-image-overlay"></div>
+        <div class="intro-image-content">
+            <h1 class="intro-image-title">US Based Software Developers</h1>
+            <p class="intro-image-subtitle">Your Source for On Demand Custom Software</p>
+            <div class="button">
+                <a href="/contact" >
+                    <button class="btn btn-primary">Get Clarity On Your Project</button>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<style lang="scss" scoped>
+    .intro-image-wrapper {
+        width: 100%;
+        height: 500px;
+        background: url('images/home-cover.jpg') no-repeat center center;
+        background-size: cover;
+        position: relative;
+        .intro-image-overlay {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5); // Change the opacity value to adjust the overlay
+        }
+        .intro-image-content {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            text-align: center;
+            .intro-image-title {
+                font-size: 3.5rem;
+                font-weight: 700;
+                margin-bottom: 1rem;
+                color: var(--color-primary); // Change the color to match your design
+            }
+            .intro-image-subtitle {
+                font-size: 2rem;
+                font-weight: 600;
+                color: var(--color-primary); // Change the color to match your design
+            }
+        }
+    }
+</style>
